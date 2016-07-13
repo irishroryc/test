a651f1bdc4ed2b80963199c121c6c75cbcbe4c56
Rory <Rory@10.197.71.59>
2016-07-13 14:51:30 -0400

    New commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: Rory
+ *
+ * Created on July 13, 2016, 2:48 PM
+ */
+
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "This is a GIT test.\n";
+    
+    return 0;
+}
+
